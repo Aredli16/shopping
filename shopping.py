0c4311df3635dc9d34cfdeb8fefc0c7c5e8b3a28
Lempereur Corentin <73390812+Aredli16@users.noreply.github.com>
2023-11-27 09:33:49 +0100

    Refactor load_data

--- shopping.py
+++ shopping.py
@@ -32,34 +32,35 @@ def main():
 
 
 def load_data(filename):
-    """
-    Load shopping data from a CSV file `filename` and convert into a list of
-    evidence lists and a list of labels. Return a tuple (evidence, labels).
+    evidence = []
+    labels = []
 
-    evidence should be a list of lists, where each list contains the
-    following values, in order:
-        - Administrative, an integer
-        - Administrative_Duration, a floating point number
-        - Informational, an integer
-        - Informational_Duration, a floating point number
-        - ProductRelated, an integer
-        - ProductRelated_Duration, a floating point number
-        - BounceRates, a floating point number
-        - ExitRates, a floating point number
-        - PageValues, a floating point number
-        - SpecialDay, a floating point number
-        - Month, an index from 0 (January) to 11 (December)
-        - OperatingSystems, an integer
-        - Browser, an integer
-        - Region, an integer
-        - TrafficType, an integer
-        - VisitorType, an integer 0 (not returning) or 1 (returning)
-        - Weekend, an integer 0 (if false) or 1 (if true)
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            evidence.append([
+                int(row[0]),  # Administrative
+                float(row[1]),  # Administrative_Duration
+                int(row[2]),  # Informational
+                float(row[3]),  # Informational_Duration
+                int(row[4]),  # ProductRelated
+                float(row[5]),  # ProductRelated_Duration
+                float(row[6]),  # BounceRates
+                float(row[7]),  # ExitRates
+                float(row[8]),  # PageValues
+                float(row[9]),  # SpecialDay
+                ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].index(row[10]),  # Month
+                int(row[11]),  # OperatingSystems
+                int(row[12]),  # Browser
+                int(row[13]),  # Region
+                int(row[14]),  # TrafficType
+                1 if row[15] == 'Returning_Visitor' else 0,  # VisitorType
+                1 if row[16] == 'TRUE' else 0  # Weekend
+            ])
+            labels.append(1 if row[17] == 'TRUE' else 0)  # Revenue
 
-    labels should be the corresponding list of labels, where each label
-    is 1 if Revenue is true, and 0 otherwise.
-    """
-    raise NotImplementedError
+    return evidence, labels
 
 
 def train_model(evidence, labels):
